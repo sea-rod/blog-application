924b0acf2cb6a0b4902fe92079fa1f5f78317a64
Seamus <Sea11rod2@gmail.com>
2023-01-12 16:49:56 +0530

    update detele.html

--- blog/settings.py
+++ blog/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG")
+DEBUG = True  # env.bool("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1"]  # "sea11.pythonanywhere.com"
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
