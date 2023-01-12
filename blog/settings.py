3e551550deefb7ea44c1721f008d0d2f43966fbd
Seamus <Sea11rod2@gmail.com>
2023-01-12 17:17:43 +0530

    add testcase

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # env.bool("DEBUG")
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test1@gmail.com", password="passs123"
+        )
+        cls.post = Post.objects.create(
+            title="testpost", body="test 1 test2", author=cls.user
+        )
+        return super().setUpTestData()
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "testpost")
+        self.assertEqual(self.post.body, "test 1 test2")
+        self.assertEqual(self.post.author.username, "testuser")
+
+    def test_url_exists_at_correct_location_detailview(self):
+        response = self.client.get("/articles/1/detail/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "test 1 test2")
+        self.assertTemplateUsed(response, "home.html")
